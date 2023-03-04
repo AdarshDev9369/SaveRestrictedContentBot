75db6a539fe9d463bde93a0465785e775533516b
AdarshDev9369 <84713294+AdarshDev9369@users.noreply.github.com>
2023-03-04 17:24:51 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = config("4069594", default=None, cast=int)
+API_HASH = config("3badefa1852d129109faa0d4d69ed251", default=None)
+BOT_TOKEN = config("6171876157:AAGCuo8jSiKq-k3lVhDtnEOFr14vcxnvvKM", default=None)
+SESSION = config("BQC-S5ZZ7HMwPCdkDUTK3nmzqqCuB72UojoaFxwInmkqCaLH_jg68g1Q5pm5bsTd8sYEA8Mt6z_VgBirBsIp1Dy5ARhRoBleZeKhXZ2e9liCn8jMBf0NPouNyneWiXnvb7aDD1SHLJJmbPZX9awuE0PsZzojQNgIqKj4FS95VWfecnkHh6Or6UTBvPyN03YY_Y-JvotsGSl6H50pXpakFFHxfADaKQqDWIlTdXTpLfICIMqWdTuTI8VW2ERMfFPY3PJRt4dSfXFbbUEFQXYgLlZt58ZqWOLsVr-LWNLNAmxZNGAJ5Mf_3_n6v0gUUTGBfIhOQdznZ8EYFs8oKaqjjCSPOiphEAA", default=None)
+FORCESUB = config("ADARSH_TELEGRAM_BOTS", default=None)
+AUTH = config("975855888", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
